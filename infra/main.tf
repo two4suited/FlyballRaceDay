783240eb98d58575ca26700138bfb8b4d195a5d0
Brian Sheridan <brian.sheridan@gmail.com>
2023-07-14 09:42:46 -0700

    Updated To Apply Infra

--- infra/main.tf
+++ infra/main.tf
@@ -17,6 +17,11 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "resourcegroup" {
     name     = "${var.application_name}-${var.environment}"
     location = var.location
-    tags = local.common_tags
-  
+    tags = local.common_tags  
+}
+
+resource "azurerm_static_site" "staticwebapp" {
+  name                = "${var.application_name}-${var.environment}"
+  resource_group_name = azurerm_resource_group.resourcegroup
+  location            = var.location
 }
