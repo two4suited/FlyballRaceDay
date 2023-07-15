86f49450bc2eaa69ea77e08632e46ef0cd170909
Brian Sheridan <brian.sheridan@gmail.com>
2023-07-14 17:04:36 -0700

    fixed naming

--- infra/apis/main.tf
+++ infra/apis/main.tf
@@ -1,7 +1,7 @@
 module "saif-apimanagement-apis" {
   source            = "../apim-apis"
   APIName           = "api"
-  OpenAPISpecFile   = file("${path.root}/apis/FlyballRaceDay.yml")
+  OpenAPISpecFile   = file("${path.root}/apis/flyballraceday.yml")
   Path              = "api"
   DisplayName       = "API's for FlyballRaceDay"
   RevisionID        = 1
