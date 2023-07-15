69922059036590c31d8a5517fb2aaa8ad4ea9ec8
Brian Sheridan <brian.sheridan@gmail.com>
2023-07-14 20:20:13 -0700

    Updated Mocking

--- infra/apis/locals.tf
+++ infra/apis/locals.tf
@@ -8,6 +8,62 @@ locals {
       mockingResponseCode = "200"
       backendService      = ""
       backendRewrite      = ""    
+    },
+    {
+      operationid         = "createTournament"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "updateTournament"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "deleteTournament"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "createSchedule"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "getschedulebyid"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "getraces"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "racemarkasdone"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
+    },
+    {
+      operationid         = "raceupnext"
+      mocking             = true
+      mockingResponseCode = "200"
+      backendService      = ""
+      backendRewrite      = ""    
     }
     ]
   }
