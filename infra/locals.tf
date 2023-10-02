9ddb415eff270273b1e4deaa0ea9ce267b302129
Brian Sheridan <brian.sheridan@gmail.com>
2023-10-02 12:49:48 -0700

    removed tags

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,14 +1,6 @@
 locals {
   common_tags = {
-     BranchName  = var.branchName
-     CommitId    = var.commitId
-     User        = var.deployedBy
-     BuildId     = var.buildId
-     BuildNumber = var.buildNumber
-     Repository  = var.repository
-     AppName     = var.application_name
-     Environment = var.environment
-     DeployTime  = timestamp()
+     User        = var.deployedBy          
   }
 }
 
