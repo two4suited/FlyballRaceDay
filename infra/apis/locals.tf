0083a5da8118ae8df7bd26e46e603201607a47fc
Brian Sheridan <brian.sheridan@gmail.com>
2023-10-02 13:36:48 -0700

    removed /

--- infra/apis/locals.tf
+++ infra/apis/locals.tf
@@ -21,7 +21,7 @@ locals {
       mocking             = false
       mockingResponseCode = "200"
       backendService      = local.tournamentBackendService
-      backendRewrite      = "/{id}"    
+      backendRewrite      = "{id}"    
     },
     {
       operationid         = "deleteTournament"
