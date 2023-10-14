e473344f28e1c78fa1ff1b432329ab055d0fecd0
Brian Sheridan <brian.sheridan@gmail.com>
2023-10-14 16:34:18 -0700

    Added Test for getting active tournaments

--- tests/Functions.Tests.Integration/TournamentsAPI/GetAllActiveTournamentsTests.cs
+++ tests/Functions.Tests.Integration/TournamentsAPI/GetAllActiveTournamentsTests.cs
@@ -32,4 +32,33 @@ public class GetAllActiveTournamentsTests  : BaseTournamentApiTests
 
         result.StatusCode.ShouldBe(HttpStatusCode.OK);
     }
+    
+    [Fact]
+    public async Task GetAllActiveTournaments_ShouldReturnOneTournament_WithTwoTournamentsOneInPast()
+    {
+        //Arrange
+        var tournament1 = _tournamentDataGenerator.Generate();
+        var tournament2 = _tournamentDataGenerator.Generate();
+        tournament2.StartDate = tournament2.StartDate.AddDays(-10);
+        tournament2.EndDate= tournament2.EndDate.AddDays(-10);
+        
+        var mongoClient = new MongoClient(
+            _factory.OptionsForDatabase.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            _factory.OptionsForDatabase.Value.DatabaseName);
+
+        var collection = mongoDatabase.GetCollection<TournamentDataModel>(
+            _factory.OptionsForDatabase.Value.CollectionName);
+        
+        await collection.InsertOneAsync(tournament1);
+        await collection.InsertOneAsync(tournament2);
+        
+        //Act
+        var result = await _fut.GetAllActiveTournaments(_factory.Request);
+        var returnedTournaments = TestHelpers.DeserializeHttpResponseData<List<Tournament>>(result);
+        
+
+        returnedTournaments.Count.ShouldBe(1);
+    }
 }
